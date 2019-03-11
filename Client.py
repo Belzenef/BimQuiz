9223d9d5e76ccfc2eeb58023e5c5c5b88a076701
Elise Jacquemet <elise.jacquemet@insa-lyon.fr>
2019-03-11 11:55:40 +0100

    quizz almost working, need to add questions to partie

--- Client.py
+++ Client.py
@@ -19,65 +19,70 @@ comSocket = socket(AF_INET, SOCK_STREAM)
 comSocket.setsockopt(SOL_SOCKET, SO_REUSEADDR, 1)
 
 class Client:
-	def __init__(self,ch):
-		# Attributs
-		self.name = ch
-		self.sock = socket(AF_INET, SOCK_STREAM)
-		self.sock.connect(('127.0.0.1',8000))
-		self.sock.sendall(name.encode('ascii'))
-		response = sock.recv(1024)
-		connected=True
-		if not response : 
-			print("Server has been deconnected")
-			connected=False
-		else : 
-			response=response.decode('ascii')
-			print("Welcome, %s ! :) \nTo start a new Quizz enter 'start'" % response)
-			print("To wait for firends, enter 'wait' ")
-			print("To quit server, enter 'quit' ")
-		while connected :
-			line=input(">")
-			line+="\x00"
-			if line == "quit\x00" : 
-				print("Ending connection")
-				sock.sendall(line.encode('ascii'))
-				connected=False
-			elif line == "wait\x00" :
-				response = sock.recv(1024)
-				if not response : 
-					print("Server has been deconnected")
-					connected=False
-				else : 
-					response=response.decode('ascii')
-					print(response)
-			elif line == "start\x00" :
-				line=line.encode('ascii')
-				sock.sendall(line)
-				response = sock.recv(1024)
-				if not response : 
-					print("Server has been deconnected")
-					connected=False
-				else : 
-					response=response.decode('ascii')
-					print(response)
- 			else :
- 				print("Sorry, I didn't get what you said ?")
-				
-		sock.close()
+    def __init__(self,ch):
+        # Attributs
+        self.name = ch
+        self.connected=False
+        print("Waiting connection...")
+        self.sock = socket(AF_INET, SOCK_STREAM)
+        self.sock.connect(('127.0.0.1',8000))
+        self.sock.sendall(self.name.encode('ascii'))
+        response = self.sock.recv(1024).decode('ascii')
+        print("Welcome, %s ! :) \nTo start a new Quizz enter 'start'" % response)
+        print("To wait for friends, enter 'wait' ")
+        print("To quit server, enter 'quit' ")
+        self.connected=True
+        inp='1'
+        try :
+            while self.connected :
+                if inp=='1' :
+                    line=input(">")
+                    line+="\x00"
+                    if line == "quit\x00" : 
+                        print("Ending connection")
+                        self.sock.sendall(line.encode('ascii'))
+                        self.connected=False
+                    elif line == "wait\x00" :
+                        inp=self.lire(self.sock)
+                    elif line == "start\x00" :
+                        line=line.encode('ascii')
+                        self.sock.sendall(line)
+                        inp=self.lire(self.sock)
+                    else :
+                        self.sock.sendall(line)
+                        inp=self.lire(self.sock)
+                else : 
+                    inp=self.lire(self.sock)
+        except KeyboardInterrupt :
+            print("Ending connection")
+            line = "quit\x00"
+            self.sock.sendall(line.encode('ascii'))
+        finally :
+            self.sock.close()
+
+    def lire(self,sock):
+        res = '0' # entier (0 si pas de réponse attendue, 1 sinon)
+        response = sock.recv(1024).decode('ascii')
+        if not response : 
+            print("Server has been deconnected")
+            self.connected=False
+        print(response[:-1])
+        res=response[-1]
+        return res
 
 if __name__ == "__main__":
-	try:
-		if len(sys.argv)>=2 :
-			ch=sys.argv[1]
-		else : 
-			ch=input('Choose nickname > ')
-		while ch=="" :
-			print("Sorry this nickname is not available !")
-			ch=input('Choose nickname > ')
-		Client(ch)
-	except KeyboardInterrupt :
-		print("Quiting server")
-	except:
-		print("Unexpected exception ... Sorry :/")
-	finally :
-		print('Thanks for playing ! ^_^ ')
+    try:
+        if len(sys.argv)>=2 :
+                ch=sys.argv[1]
+        else : 
+            ch=input('Choose nickname > ')
+        while ch=="" :
+            print("Sorry this nickname is not available !")
+            ch=input('Choose nickname > ')
+        Client(ch)
+    except KeyboardInterrupt :
+        print("Quiting server")
+    except:
+        print("Unexpected exception ... Sorry :/")
+    finally :
+        print('Thanks for playing ! ^_^ ')
--- Serveur.py
+++ Serveur.py
@@ -13,102 +13,108 @@ comSocket.setsockopt(SOL_SOCKET, SO_REUSEADDR, 1)
 
 
 class Serveur:
-	def __init__(self):
-		# Initialisation de la classe """
-		manager = Manager()
-		self.clients = {}
-		self.TAILLE_BLOC=1024 # la taille des blocs 
-		self.sock = socket(AF_INET, SOCK_STREAM)
-		self.sock.bind(("127.0.0.1",8000))
-		self.sock.listen(5)
-		self.partie_lancee=Value('i', 0)
-		self.partie_en_cours=Value('i', 0)
-		self.queue = Queue()
-		queue.put(self.clients)
-		print("Welcome to the best Quiz ever! ")
-		self.run()
-		
-	def run(self):
-		while True:
-			con, addr = self.sock.accept()
-			process = Process(target=self.handle_com, args=(con, addr, self.queue))
-			process.daemon = True
-			process.start()
-			#if self.partie_lancee.value==1 :
-			#	print("inside if")
-				#game = multiprocessing.Process(target=self.partie, args=(self.clients))
-				#game.daemon = True
-				#game.start()
-			#	self.partie_lancee.acquire()
-			#	self.partie_lancee.value=0
-			#	self.partie_lancee.release()
-			#	self.partie_en_cours.acquire()
-			#	self.partie_en_cours.value=1
-			#	self.partie_en_cours.release()
-			#	self.partie(self.clients)
-		self.sock.close()
+    def __init__(self):
+        # Initialisation de la classe """
+        self.TAILLE_BLOC=1024 # la taille des blocs 
+        self.sock = socket(AF_INET, SOCK_STREAM)
+        self.sock.bind(("127.0.0.1",8000))
+        self.sock.listen(5)
+        self.partie_lancee=Value('i', 0)
+        self.partie_en_cours=Value('i', 0)
+        self.queue = Queue()
+        print("Welcome to the best Quiz ever! ")
+        self.run()
 
-		
-	def handle_com(self, sockClient, addr, queue):
-		connected = True
-		try:
-			pseudo = sockClient.recv(self.TAILLE_BLOC)
-			pseudo=pseudo.decode("ascii")
-			queue.get()[sockClient]=pseudo
-			print("%s joined server" % pseudo)
-			response=pseudo.encode("ascii")
-			sockClient.sendall(response)
-			while connected:
-				data = sockClient.recv(self.TAILLE_BLOC)
-				data=data.decode('ascii')
-				if data ==  "quit\x00":
-					print("%s has left server :'(" %pseudo)
-					connected=False
-				elif data == "start\x00":
-					if self.partie_en_cours.value==0 :
-						print("Starting game ...")
-						response="You started a new game !\n"
-						response=response.encode("ascii")
-						sockClient.sendall(response)
-						self.partie_en_cours.acquire()
-						self.partie_en_cours.value=1
-						self.partie_en_cours.release()
-						#process = multiprocessing.Process(target=self.partie, args=(self.clients))
-						#process.daemon = True
-						#process.start()
-						self.partie(queue.get())
-					else :
-						ch="Game already started :/"
-						sockClient.sendall(ch.encode("ascii"))
-				else :
-					data+="\x00"
-					data=data.encode("ascii")
-					sockClient.sendall(data)
-		except:
-			print("Problem in request ?")
-		finally:
-			sockClient.close()
+    def run(self):
+        while True:
+            con, addr = self.sock.accept()
+            process = Process(target=self.handle_com, args=(con, addr, self.queue))
+            process.daemon = True
+            process.start()
+        self.sock.close()
 
+        
+    def handle_com(self, sockClient, addr, queue):
+        connected = True
+        try:
+            pseudo = sockClient.recv(self.TAILLE_BLOC)
+            pseudo=pseudo.decode("ascii")
+            queue.put(pseudo)
+            queue.put(sockClient)
+            print("%s joined server" % pseudo)
+            sockClient.sendall(pseudo.encode("ascii"))
+            while connected:
+                data = sockClient.recv(self.TAILLE_BLOC)
+                data=data.decode('ascii')
+                if data ==  "quit\x00":
+                    print("%s has left server :'(" %pseudo)
+                    connected=False
+                elif data == "start\x00":
+                    if self.partie_en_cours.value==0 :
+                        print("Starting game ...")
+                        response="You started a new game !0"
+                        sockClient.sendall(response.encode("ascii"))
+                        self.partie_en_cours.acquire()
+                        self.partie_en_cours.value=1
+                        self.partie_en_cours.release()
+#                        game = multiprocessing.Process(target=self.partie, args=(queue))
+#                        game.daemon = True
+#                        game.start()
+                        self.partie(queue)
+                    else :
+                        response="Game already started :/0"
+                        sockClient.sendall(response.encode("ascii"))
+        except:
+            print("Problem in request ?")
+        finally:
+            sockClient.close()
 
-	def partie(self, joueurs):
-		print("Connected players : ")
-		print(joueurs)
-		#for sock in joueurs.keys():
-		#	print(joueurs[sock])
-		#	response="Welcome to the party"
-		#	response=response.encode("ascii")
-		#	sock.sendall(response)
+    def partie(self, queue):
+        
+        # Récupération des joueurs connectés
+        queue.put("Done")
+        joueurs={} # joueurs[sock]=(pseudo,score)
+        print("Connected players : ")
+        while True :
+            msg=queue.get()
+            if msg=="Done" :
+                break
+            elif type(msg)==type("") :
+                pseudo=msg
+                sock=queue.get()
+                score=0
+                joueurs[sock]=(pseudo,score)
+                print(pseudo)
+        response="Welcome to the party o/ 0"
+        response=response.encode("ascii")
+        for sock in joueurs.keys():
+            sock.send(response)
+        
+        # Récupération des questions
+        
+        # Déroulé du quizz
+        
+        # Affichage des scores
+        
+        # Fin de la partie
+        self.partie_en_cours.acquire()
+        self.partie_en_cours.value=0
+        self.partie_en_cours.release()
+        response="Thanks for playing ;)\n1"
+        response=response.encode("ascii")
+        for sock in joueurs.keys():
+            sock.send(response)
  
 if __name__ == "__main__":
-		try:
-			Serveur()
-		except:
-			print("Unexpected exception")
-		finally:
-			print("Shutting down")
-			for process in active_children():
-				print("Shutting down process ", process)
-				process.terminate()
-				process.join()
-			print("END")
-	
+        try:
+            Serveur()
+        except KeyboardInterrupt :
+            print("Shutting down server")
+        except:
+            print("Unexpected exception")
+        finally:
+            for process in active_children():
+                print("Shutting down process ", process)
+                process.terminate()
+                process.join()
+            print("END")
